862f4d20081107038646e44b2427010f2d2085b7
shatakshi-1404 <escape>
2026-03-06 16:25:06 +0530

    env update

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-app-backend-vzq3.onrender.com");
+const socket = io(import.meta.env.VITE_SOCKET_URL);
+
 
 export default socket;
